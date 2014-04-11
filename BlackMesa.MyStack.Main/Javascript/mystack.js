30a160e54b3e9b730dbe3986a17dd109a58138c7
Stephan Sum <sum@live.de>
2014-04-11 20:07:41 +0200

    Finished partial ajax (de)select funtionality; Improved menus; Fixed some ajax related bugs

--- BlackMesa.MyStack.Main/Javascript/mystack.js
+++ BlackMesa.MyStack.Main/Javascript/mystack.js
@@ -1,1 +1,42 @@
 ﻿
+var toggleMenuOptions = function() {
+    if ($("a.checked").length > 0) {
+        $("#has-any-selection-menu").show();
+        $("#has-none-selection-menu").hide();
+    } else {
+        $("#has-any-selection-menu").hide();
+        $("#has-none-selection-menu").show();
+    }
+};
+
+var addCardSuccess = function (result) {
+    toggleMenuOptions();
+};
+
+var removeCardSuccess = function (result) {
+    toggleMenuOptions();
+};
+
+var addFolderSuccess = function (result) {
+    toggleMenuOptions();
+};
+
+var removeFolderSuccess = function (result) {
+    toggleMenuOptions();
+};
+
+$(document).ready(function () {
+
+    toggleMenuOptions();
+
+    //$("div[id$='-checkbox']").click(function () {
+    //    if ($("a.checked").length > 0) {
+    //        $("#has-any-selection").show();
+    //        $("#has-none-selection").hide();
+    //    } else {
+    //        $("#has-any-selection").hide();
+    //        $("#has-none-selection").show();
+    //    }
+    //});
+
+});
