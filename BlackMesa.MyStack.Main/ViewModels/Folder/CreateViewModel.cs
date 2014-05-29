432e8761867de99812734b89c9fcadb4b4f12eb5
Stephan Sum <sum@live.de>
2014-05-29 14:39:57 +0200

    Added string length constraint for folder names to view model

--- BlackMesa.MyStack.Main/ViewModels/Folder/CreateViewModel.cs
+++ BlackMesa.MyStack.Main/ViewModels/Folder/CreateViewModel.cs
@@ -9,6 +9,7 @@ namespace BlackMesa.MyStack.Main.ViewModels.Folder
         public string ParentFolderId { get; set; }
 
         [Required]
+        [StringLength(50)]
         public string Name { get; set; }
 
 
