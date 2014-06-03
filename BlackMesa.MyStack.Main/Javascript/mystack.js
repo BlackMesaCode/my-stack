c7caa6ce67632f0ae0e2a436e2e62749a37ce4c6
Stephan Sum <sum@live.de>
2014-06-03 12:18:12 +0200

    Disabled all hover effects when user is on a touch device

---
+++ BlackMesa.MyStack.Main/Javascript/mystack.js
@@ -0,0 +1,12 @@
+﻿
+function isTouchDevice() {
+    return 'ontouchstart' in window || !!(navigator.msMaxTouchPoints);
+}
+
+$(document).ready(function () {
+
+    if (isTouchDevice())    {
+        $('body').removeClass('no-touch');
+    }
+
+});
